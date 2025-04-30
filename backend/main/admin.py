f36867f667dfb5f73ae59aa1b9504686f2696a7b
Olga Belova <obelova@gmcs.ru>
2025-04-30 13:46:58 +0300

    start

---
+++ backend/main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from main.models import Categories, Courses, Lessons
+
+
+admin.site.register(Categories)
+admin.site.register(Courses)
+admin.site.register(Lessons)
