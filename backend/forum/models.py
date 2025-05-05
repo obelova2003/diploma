9fdab5baa92f86931b1d08c52a8fee9138e32fbf
Olga Belova <obelova@gmcs.ru>
2025-05-05 12:33:00 +0300

    +

--- backend/forum/models.py
+++ backend/forum/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Текст поста")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор поста")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата редактирования")
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE, verbose_name="Пост")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="")
+    content = models.TextField(verbose_name="")
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE, verbose_name="")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="")
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
